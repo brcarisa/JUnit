fdbb5ca0172dff2233dca1588611b23be4034f4e
Carisa Braxton <brcarisa@ge-g6.kzn.21-school.ru>
2024-04-28 19:10:44 +0300

    connection to embedded database with Spring

---
+++ ex01/Tests/src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO butik.product (identifier, name, price) VALUES (1, 'iPhone', 100000);
+INSERT INTO butik.product (identifier, name, price) VALUES (2, 'Samsung', 88000);
+INSERT INTO butik.product (identifier, name, price) VALUES (3, 'Pixel', 77000);
+INSERT INTO butik.product (identifier, name, price) VALUES (4, 'Blackberry', 55000);
---
+++ ex01/Tests/src/test/resources/schema.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS butik;
+
+CREATE TABLE IF NOT EXISTS butik.product (identifier integer, name varchar(20), price integer);
